de19763843b7373e64a184c005143fbda30595d7
Janescobar92 <jancore92@gmail.com>
2020-12-18 19:45:45 +0000

    adding jumbotron style and content

--- src/js/component/footer.jsx
+++ src/js/component/footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export const Footer = () => {
 	return (
 		<footer className="conainer-fluid d-flex justify-content-center align-items-end bg-dark text-white mt-1 footer">
-			<p className="mb-0">CopyRight @ 2020 Your website</p>
+			<p className="mb-0">CopyRight @NotSoScaryWeb 2020</p>
 		</footer>
 	);
 };
